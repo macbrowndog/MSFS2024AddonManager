953066c354dbfe3c5be9a07f974197f797b82721
macbrowndog <andrewmacbrown@gmail.com>
2026-07-22 10:41:48 +0930

    Version 0.1.0 - Application foundation complete

--- MSFS2024AddonManager/MainForm.cs
+++ MSFS2024AddonManager/MainForm.cs
@@ -4,8 +4,24 @@ namespace MSFS2024AddonManager;
 
 public partial class MainForm : KryptonForm
 {
+    public const string ApplicationName = "MSFS 2024 Addons Manager";
+
     public MainForm()
     {
         InitializeComponent();
+
+        ConfigureWindow();
+    }
+
+    private void ConfigureWindow()
+    {
+        string version = Application.ProductVersion;
+
+        Text = $"{ApplicationName} • Version {version}";
+
+        StartPosition = FormStartPosition.CenterScreen;
+
+        MinimumSize = new Size(1200, 750);
+        Size = new Size(1500, 900);
     }
 }
